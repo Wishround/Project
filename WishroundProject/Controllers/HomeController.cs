3d84f810ec7ccbcf24adf33b01ee22790e18fdb9
Vasyl <prystoynyy@gmail.com>
2015-08-19 09:03:10 +0300

    Add bootstrap template

--- WishroundProject/Controllers/HomeController.cs
+++ WishroundProject/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WishroundProject.Controllers
 {
     public class HomeController : Controller
     {
-        [RequireHttps]
+
         public ActionResult Index()
         {
             return View();
